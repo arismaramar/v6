68ca494373fce12db34206edb3c8f3ea9c03cc1f
free net <74614880+arismaramar@users.noreply.github.com>
2023-12-25 11:58:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -121,7 +121,7 @@ echo "$localip $(hostname)" >> /etc/hosts
 fi
 mkdir -p /etc/xray
 
-echo -e "${tyblue} Welcome To Script Premium YSSHstore${NC} "
+echo -e "${tyblue} Welcome To Script Premium ANGGUN${NC} "
 sleep 2
 echo -e "[ ${green}INFO${NC} ] Mempersiapkan Instal File"
 apt install git curl -y >/dev/null 2>&1
